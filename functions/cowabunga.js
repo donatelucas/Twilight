eab368f1639c051ba3cd1df4681011872a98b33a
Lucas Perez <knightfication@gmail.com>
2019-04-28 20:08:36 -0500

    Added Menu, style changes

--- functions/cowabunga.js
+++ functions/cowabunga.js
@@ -1,7 +1,1 @@
-/*
-    #000000 - Registration Black
-    #3D2645 - Russian Violet
-    #832161 - Dark Raspberry
-    #DA4167 - Paradise Pink
-    #F0EFF4 - Anti-Flash White
-*/
+console.log("See ya, Space Cowboy");
